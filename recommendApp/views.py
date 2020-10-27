399c8aa823fb9f2eb64bb64b596f5fc6baa4782d
geonho <geonho0116@naver.com>
2020-10-27 17:17:22 +0900

    final edit

--- recommendApp/views.py
+++ recommendApp/views.py
@@ -3,11 +3,19 @@ from django.shortcuts import render
 # from Untitled import test
 from .models import Test
 
+import scipy as sp
 import pandas as pd
+import numpy as np
 import csv
 import surprise
 
 import warnings
+
+
+# Create your views here.
+
+# def bar(request):
+
 
 def recommend_index(request):
     test = Test.objects
@@ -24,13 +32,15 @@ def recommend_new(request):
         if (request.POST.get('q1') == '1'):
             if (request.POST.get('q2') == '1'):
                 if (request.POST.get('q3') == '1'):
+                    num = pd.read_csv('뚜벅혼자관광.csv', encoding="utf-8", sep=",", error_bad_lines=False)
                     f = open('뚜벅혼자관광.csv', 'a', newline='', encoding='utf-8')
                     wr = csv.writer(f)
                     wr.writerow([test.name, test.local, test.rating])
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('뚜벅혼자관광.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('뚜벅혼자관광.csv', encoding="utf-8", sep=",", error_bad_lines=False)
+
                 elif (request.POST.get('q3') == '2'):
                     f = open('뚜벅혼자휴양.csv', 'a', newline='', encoding='utf-8')
                     wr = csv.writer(f)
@@ -38,7 +48,8 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('뚜벅혼자휴양.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('뚜벅혼자휴양.csv', encoding="utf-8", sep=",", error_bad_lines=False)
+
             elif (request.POST.get('q2') == '2'):
                 if (request.POST.get('q3') == '1'):
                     f = open('뚜벅2인관광.csv', 'a', newline='', encoding='utf-8')
@@ -47,7 +58,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('뚜벅2인관광.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('뚜벅2인관광.csv', encoding="utf-8", sep=",", error_bad_lines=False)
 
                 elif (request.POST.get('q3') == '2'):
                     f = open('뚜벅2인휴양.csv', 'a', newline='', encoding='utf-8')
@@ -56,7 +67,8 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('뚜벅2인휴양.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('뚜벅2인휴양.csv', encoding="utf-8", sep=",", error_bad_lines=False)
+
             elif (request.POST.get('q2') == '3'):
                 if (request.POST.get('q3') == '1'):
                     f = open('뚜벅3인관광.csv', 'a', newline='', encoding='utf-8')
@@ -65,7 +77,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('뚜벅3인관광.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('뚜벅3인관광.csv', encoding="utf-8", sep=",", error_bad_lines=False)
 
                 elif (request.POST.get('q3') == '2'):
                     f = open('뚜벅3인휴양.csv', 'a', newline='', encoding='utf-8')
@@ -74,7 +86,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('뚜벅3인휴양.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('뚜벅3인휴양.csv', encoding="utf-8", sep=",", error_bad_lines=False)
 
         elif (request.POST.get('q1') == '2'):
             if (request.POST.get('q2') == '1'):
@@ -85,7 +97,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('자차혼자관광.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('자차혼자관광.csv', encoding="utf-8", sep=",", error_bad_lines=False)
                 elif (request.POST.get('q3') == '2'):
                     f = open('자차혼자휴양.csv', 'a', newline='', encoding='utf-8')
                     wr = csv.writer(f)
@@ -93,7 +105,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('자차혼자휴양.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('자차혼자휴양.csv', encoding="utf-8", sep=",", error_bad_lines=False)
             elif (request.POST.get('q2') == '2'):
                 if (request.POST.get('q3') == '1'):
                     f = open('자차2인관광.csv', 'a', newline='', encoding='utf-8')
@@ -102,7 +114,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('자차2인관광.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('자차2인관광.csv', encoding="utf-8", sep=",", error_bad_lines=False)
 
                 elif (request.POST.get('q3') == '2'):
                     f = open('자차2인휴양.csv', 'a', newline='', encoding='utf-8')
@@ -111,7 +123,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('자차2인휴양.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('자차2인휴양.csv', encoding="utf-8", sep=",", error_bad_lines=False)
             elif (request.POST.get('q2') == '2'):
                 if (request.POST.get('q3') == '3'):
                     f = open('자차3인관광.csv', 'a', newline='', encoding='utf-8')
@@ -120,7 +132,7 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('자차3인관광.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('자차3인관광.csv', encoding="utf-8", sep=",", error_bad_lines=False)
 
                 elif (request.POST.get('q3') == '2'):
                     f = open('자차3인휴양.csv', 'a', newline='', encoding='utf-8')
@@ -129,66 +141,68 @@ def recommend_new(request):
                     f.close()
 
                     warnings.filterwarnings('ignore')
-                    data = pd.read_csv('자차3인휴양.csv', encoding="utf-8", sep=",")
+                    data = pd.read_csv('자차3인휴양.csv', encoding="utf-8", sep=",", error_bad_lines=False)
 
-        df = data[['id', '여행지', 'rating']]
-        df = df.drop_duplicates(['id', '여행지', 'rating'], keep="last")
+    if (len(data) == 900000):
+        data.drop([data.index[1]])
+    df = data[['id', '여행지', 'rating']]
+    df = df.drop_duplicates(['id', '여행지', 'rating'], keep="last")
 
-        def recur_dictify(frame):
-            if len(frame.columns) == 1:
-                if frame.values.size == 1: return frame.values[0][0]
-                return frame.values.squeeze()
-            grouped = frame.groupby(frame.columns[0])
-            d = {k: recur_dictify(g.iloc[:, 1:]) for k, g in grouped}
-            return d
+    def recur_dictify(frame):
+        if len(frame.columns) == 1:
+            if frame.values.size == 1: return frame.values[0][0]
+            return frame.values.squeeze()
+        grouped = frame.groupby(frame.columns[0])
+        d = {k: recur_dictify(g.iloc[:, 1:]) for k, g in grouped}
+        return d
 
-        df_to_dict = recur_dictify(df)
+    df_to_dict = recur_dictify(df)
 
-        name_list = []
-        cos_set = set()
+    name_list = []
+    local_set = set()
 
-        for user_key in df_to_dict:
-            name_list.append(user_key)
-            for cos_key in df_to_dict[user_key]:
-                cos_set.add(cos_key)
+    for user_key in df_to_dict:
+        name_list.append(user_key)
+        for local_key in df_to_dict[user_key]:
+            local_set.add(local_key)
 
-        local_list = list(cos_set)
+    local_list = list(local_set)
 
-        rating_dic = {
-            'id': [],
-            '여행지': [],
-            'rating': []
-        }
+    rating_dic = {
+        'id': [],
+        '여행지': [],
+        'rating': []
+    }
 
-        for name_key in df_to_dict:
-            for cos_key in df_to_dict[name_key]:
-                a1 = name_list.index(name_key)
-                a2 = local_list.index(cos_key)
-                a3 = df_to_dict[name_key][cos_key]
+    for name_key in df_to_dict:
+        for cos_key in df_to_dict[name_key]:
+            a1 = name_list.index(name_key)
+            a2 = local_list.index(cos_key)
+            a3 = df_to_dict[name_key][cos_key]
 
-                rating_dic['id'].append(a1)
-                rating_dic['여행지'].append(a2)
-                rating_dic['rating'].append(a3)
+            rating_dic['id'].append(a1)
+            rating_dic['여행지'].append(a2)
+            rating_dic['rating'].append(a3)
 
-        df = pd.DataFrame(rating_dic)
+    df = pd.DataFrame(rating_dic)
 
-        reader = surprise.Reader(rating_scale=(1, 10))
-        data = surprise.Dataset.load_from_df(df[['id', '여행지', 'rating']], reader)
+    reader = surprise.Reader(rating_scale=(1, 5))
+    data = surprise.Dataset.load_from_df(df[['id', '여행지', 'rating']], reader)
 
-        trainset = data.build_full_trainset()
-        option = {'name': 'pearson'}
-        algo = surprise.KNNBasic(sim_options=option)
+    trainset = data.build_full_trainset()
+    option = {'name': 'pearson'}
+    algo = surprise.KNNBasic(sim_options=option)
 
-        algo.fit(trainset)
+    algo.fit(trainset)
 
-        index = name_list.index(test.name)
-        result = algo.get_neighbors(index, k=2)
+    index = name_list.index(test.name)
+    result = algo.get_neighbors(index, k=3)
 
-        def localtest():
-            for r1 in result:
-                max_rating = data.df[data.df["id"] == r1]["rating"].max()
-                local_id = data.df[(data.df["rating"] == max_rating) & (data.df["id"] == r1)]["여행지"].values
-                for local_item in local_id:
-                    return (local_list[local_item])
+    def localtest():
+        for r1 in result:
+            max_rating = data.df[data.df["id"] == r1]["rating"].max()
+            local_id = data.df[(data.df["rating"] == max_rating) & (data.df["id"] == r1)]["여행지"].values
+            for local_item in local_id:
+                return (local_list[local_item])
 
-        return render(request, 'recommend_result.html', {'localtest': localtest})
+    return render(request, 'recommend_result.html', {'localtest': localtest})
